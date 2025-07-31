cae0fabd0d8cff5b68d472d920a70d7dd873d526
creatorh3 <creatorh35@gmail.com>
2025-07-31 18:40:51 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -1,21 +1,26 @@
 // config.js
-require('dotenv').config(); // লোকালে .env ফাইল থেকে তথ্য পড়ার জন্য
+
+// .env ফাইল লোড করার জন্য (এটি আপনার নিজের কম্পিউটারে বট চালানোর সময় কাজে লাগবে)
+require('dotenv').config();
 
 module.exports = {
-  // --- গোপন তথ্য (Render-এর Environment Variables থেকে আসবে) ---
+  // --- গোপন তথ্য (এইগুলো Render-এর Environment Variables থেকে লোড হবে) ---
+  // সরাসরি এখানে কোনো গোপন টোকেন বা কী লিখবেন না।
   botToken: process.env.BOT_TOKEN,
   cronSecret: process.env.CRON_SECRET,
   firebaseServiceAccount: process.env.FIREBASE_SERVICE_ACCOUNT,
 
   // --- লিঙ্ক ও মিডিয়া ---
   links: {
-    welcomeImage: 'https://i.postimg.cc/RZJLjTJ9/retouch-2025073115005700.jpg',
-    viralVideoImage: 'https://i.postimg.cc/WbsVDL3h/20250731-122806.jpg',
+    welcomeImage: 'https://i.postimg.cc/RZJLjTJ9/retouch-2025_07_31_15_00_57_00.jpg',
+    viralVideoImage: 'https://i.postimg.cc/WbsVDL3h/20250731_122806.jpg',
     downloadLink: 'https://amhubltd.blogspot.com',
-    miniAppLink: 'http://t.me/HubCoin_minerbot?startapp',
+    // FIX: http:// কে https:// করা হয়েছে
+    miniAppLink: 'https://t.me/HubCoin_minerbot?startapp',
   },
   
-  botUsername: 'Watermelonvideo_bot', // আপনার বটের ইউজারনেম (লিঙ্ক তৈরির জন্য)
+  // --- সাধারণ সেটিংস ---
+  botUsername: 'Watermelonvideo_bot', // আপনার বটের ইউজারনেম (রেফারেল লিঙ্ক তৈরির জন্য)
 
   // --- ব্যবসার নিয়মকানুন (Business Logic) ---
   referralBonus: 25,
